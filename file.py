337a5d8bc6f6031657d601c7a8cbfe30dad5ca40
mahinfah <mahinfah1234@gmail.com>
2024-08-09 00:07:28 -0800

    NEW FILE CREATE AN WRITE AND APPEND THINGS OUT

--- file.py
+++ file.py
@@ -4,4 +4,9 @@ print (r.read())
 
 # file writing
 newfile=open("new.txt","w")
-newfile.write("hell ywasewasdadadadhd/nieaw98jsas a/nsajoskasii")
+newfile.write("hello everyone \nToday is very special \nBecause Dr younus came to bd and take his oath")
+newfile.write("hell ywasewasdadadadhd\nHope everyone Will be happy ")
+
+# file appending
+neww = open("new.txt", "a")
+neww.write("nOW LET'S SEE N HOPE FOR THE BEST")
