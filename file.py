af2363549882cf10d6d9610312b1de58a0e80fac
mahinfah <mahinfah1234@gmail.com>
2024-08-09 00:22:16 -0800

    asa

--- file.py
+++ file.py
@@ -4,9 +4,13 @@ print (r.read())
 
 # file writing
 newfile=open("new.txt","w")
-newfile.write("hello everyone \nToday is very special \nBecause Dr younus came to bd and take his oath")
-newfile.write("hell YEAH\nHope everyone Will be happy ")
+newfile.write("hello everyone \nToday is very special \nBecause Dr younus came to bd and take his oath \n")
+newfile.write("Hope everyone Will be happy ")
 
 # file appending
 neww = open("new.txt", "a")
 neww.write("nOW LET'S SEE N HOPE FOR THE BEST")
+
+import os
+
+os.remove("lol.txt")
