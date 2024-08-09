578a7b212951cd1f1355dbe4f2479c42933bb8d0
mahinfah <mahinfah1234@gmail.com>
2024-08-08 23:50:42 -0800

    new file

---
+++ file.py
@@ -0,0 +1,7 @@
+# file openning reading
+r= open("main.html", "r")
+print (r.read())
+
+# file writing
+newfile=open("new.txt","w")
+newfile.write("hell ywasewasdadadadhd/nieaw98jsas a/nsajoskasii")
