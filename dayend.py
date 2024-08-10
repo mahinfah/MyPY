62f8c0c87ee5cd0ccab47abb96b8fdee65ef3eb2
mahinfah <mahinfah1234@gmail.com>
2024-08-09 21:17:49 -0800

    CONSSN

--- dayend.py
+++ dayend.py
@@ -1,6 +1,55 @@
-print("Hello")
-x = input("Enter your name: ")
-print("nice to meet you "+ x)
+#...
+#print("Hello")
+#x = input("Enter your name: ")
+#print("nice to meet you "+ x)
 
-Y=input("Enter your age: ")
-print("nice to meet you at "+Y+"  age")
+#Y=input("Enter your age: ")
+#print("nice to meet you at "+Y+"  age")
+
+
+
+#method
+#class nice_tomeetyou: 
+ 
+ #def familty(self,name,age):
+    #print("hello "+name+" nice to meet you at "+age+" age")
+
+
+ 
+ 
+ 
+#p1= nice_tomeetyou()
+#p1.familty("John",22)
+
+
+#constructror
+
+#class mylife:
+
+ # def __init__(self,age ,name):
+
+#  print("Hello "+name+" nice to meet you at "+age+" age")
+
+
+
+
+#test = mylife("22","mahin")    
+
+
+
+#TAKING USER INPUT IN A  CONSTRUCTOR
+
+#class faculty:
+   # def __init__(self):
+      #  self.name = input("Enter your name: ")
+     #   self.age = input("Enter your age: ")
+    #    self.department = input("Enter your department: ")
+   #     print("name: "+self.name)
+  #     print("age: "+self.age)
+ #       print("department: "+self.department)
+
+
+
+
+#test=faculty()
+
