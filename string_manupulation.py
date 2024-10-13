55ab05a165e05ffeca02f340bc8e0d7feee17d6e
mahin <mahinfah1234@gmail.com>
2024-10-13 21:11:59 +0600

    okok

---
+++ string_manupulation.py
@@ -0,0 +1,17 @@
+#array
+cars = ["Ford", "Volvo", "BMW"]
+for i in cars:
+    print(i)
+
+
+#string
+a="Hello, World!"
+
+print(a[1])
+
+print(a[-2])
+#printing from last 2nd character
+
+
+#printing only hello
+print(a[0:5])
