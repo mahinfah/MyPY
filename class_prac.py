32e8b89868913d155fec6706ef85502b255f6db4
mahin <mahinfah1234@gmail.com>
2024-10-16 20:35:00 +0600

    ss

--- class_prac.py
+++ class_prac.py
@@ -1,10 +1,13 @@
 class student:
-    def _init__(self,name,age,id):
-      print("This is a constructor")
+    def __init__(self,name,age,id):
+       self.name=name
+       self.age=age
+       self.id=id
+       print("This is a constructor")
 
 
 
 
 
-s1=student()
+s1=student("mahin",22,101)
 print(s1.name)
