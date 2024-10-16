13e5868b5dfa0db3547d97a67e0d20b05c552314
mahin <mahinfah1234@gmail.com>
2024-10-16 20:45:44 +0600

    ok

--- class_prac.py
+++ class_prac.py
@@ -1,13 +1,15 @@
 class student:
+#constructor must be defined with __init__ method
     def __init__(self,name,age,id):
        self.name=name
        self.age=age
        self.id=id
        print("This is a constructor")
-
-
+#methods
+    def hudai(self):
+        print("This is a method if ykyk "+self.name)
 
 
 
 s1=student("mahin",22,101)
-print(s1.name)
+print(s1.hudai())
